4227e22b95dd3483dee76c23445067abe77a16b0
onetuks <onetuks@naver.com>
2024-01-09 19:20:15 +0900

    Chore: 초기 환경 구축

--- src/main/java/com/tangerine/thymeleaf/basic/BaseController.java
+++ src/main/java/com/tangerine/thymeleaf/basic/BaseController.java
@@ -1,2 +1,8 @@
-package com.tangerine.thymeleaf.basic;public class BaseController {
+package com.tangerine.thymeleaf.basic;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "basic")
+public class BaseController {
 }
