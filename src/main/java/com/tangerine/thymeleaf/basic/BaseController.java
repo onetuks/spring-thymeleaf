1560f203e331b78dc69fe51df0cc83ea2319164b
onetuks <onetuks@naver.com>
2024-01-09 19:21:18 +0900

    텍스트 - text, utext

--- src/main/java/com/tangerine/thymeleaf/basic/BaseController.java
+++ src/main/java/com/tangerine/thymeleaf/basic/BaseController.java
@@ -1,8 +1,23 @@
 package com.tangerine.thymeleaf.basic;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(path = "basic")
 public class BaseController {
+
+    @GetMapping(path = "text-basic")
+    public String textBasic(Model model) {
+        model.addAttribute("data", "Hello Spring");
+        return "basic/text-basic";
+    }
+
+    @GetMapping(path = "text-unescaped")
+    public String textUnescaped(Model model) {
+        model.addAttribute("data", "Hello <b>Spring</b>");
+        return "basic/text-unescaped";
+    }
+
 }
